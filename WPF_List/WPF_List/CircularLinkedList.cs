ff84a16e148dea21a645a0f16890555673197f8b
Karina <105536477+KarinaShupik@users.noreply.github.com>
2023-03-26 11:19:43 +0300

    List app

---
+++ WPF_List/WPF_List/CircularLinkedList.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace WPF_List
+{
+    internal class CircularLinkedList
+    {
+        public Node Head { get; private set; }
+        public int Size { get; private set; }
+
+        public CircularLinkedList()
+        {
+            Head = null;
+            Size = 0;
+        }
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if (Head == null)
+            {
+                Head = newNode;
+                Head.Next = Head;
+            }
+            else
+            {
+                newNode.Next = Head.Next;
+                Head.Next = newNode;
+            }
+            Size++;
+        }
+
+        public void Remove(int data)
+        {
+            if (Head == null)
+                return;
+
+            Node current = Head;
+
+            do
+            {
+                if (current.Next.Data == data)
+                {
+                    current.Next = current.Next.Next;
+                    Size--;
+                    if (Head.Data == data)
+                        Head = Head.Next;
+                    return;
+                }
+                current = current.Next;
+            } while (current != Head);
+        }
+
+        public void AddBefore(int value, int newValue)
+        {
+            if (Head == null)
+                return;
+
+            Node current = Head;
+            do
+            {
+                if (current.Next.Data == value)
+                {
+                    Node newNode = new Node(newValue);
+                    newNode.Next = current.Next;
+                    current.Next = newNode;
+                    if (Head.Data == value)
+                        Head = newNode;
+                    Size++;
+                    return;
+                }
+                current = current.Next;
+            } while (current != Head);
+        }
+
+        public void SwapElements(int firstValue, int secondValue)
+        {
+            if (Head == null)
+                return;
+
+            Node firstNode = null;
+            Node secondNode = null;
+            Node current = Head;
+
+            do
+            {
+                if (current.Data == firstValue)
+                    firstNode = current;
+
+                if (current.Data == secondValue)
+                    secondNode = current;
+
+                current = current.Next;
+            } while (current != Head);
+
+            if (firstNode == null || secondNode == null)
+                return;
+
+            Node firstPrev = GetPreviousNode(firstNode);
+            Node secondPrev = GetPreviousNode(secondNode);
+
+            if (firstPrev != null)
+                firstPrev.Next = secondNode;
+            else
+                Head = secondNode;
+
+            if (secondPrev != null)
+                secondPrev.Next = firstNode;
+            else
+                Head = firstNode;
+
+            Node temp = firstNode.Next;
+            firstNode.Next = secondNode.Next;
+            secondNode.Next = temp;
+        }
+
+        private Node GetPreviousNode(Node node)
+        {
+            Node current = Head;
+
+            while (current != null && current.Next != node)
+                current = current.Next;
+
+            return current;
+        }
+
+        public void Clear()
+        {
+            Head = null;
+            Size = 0;
+        }
+
+        
+       }
+}
+
---
+++ WPF_List/WPF_List/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_List
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+        public partial class MainWindow : Window
+        {
+        private CircularLinkedList linkedList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            linkedList = new CircularLinkedList();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(addItemTextBox.Text, out int data))
+            {
+                linkedList.Add(data);
+                UpdateListBox();
+                addItemTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid integer value.");
+            }
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(removeItemTextBox.Text, out int data))
+            {
+                linkedList.Remove(data);
+                UpdateListBox();
+                removeItemTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid integer value.");
+            }
+        }
+
+        private void AddBeforeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(valueTextBox.Text, out int value) && int.TryParse(newValueTextBox.Text, out int newData))
+            {
+                linkedList.AddBefore(value, newData);
+                UpdateListBox();
+                valueTextBox.Clear();
+                newValueTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid integer values.");
+            }
+        }
+
+        private void SwapButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(firstValueTextBox.Text, out int firstValue) && int.TryParse(secondValueTextBox.Text, out int secondValue))
+            {
+                linkedList.SwapElements(firstValue, secondValue);
+                UpdateListBox();
+                firstValueTextBox.Clear();
+                secondValueTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid integer values.");
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            linkedList.Clear();
+            UpdateListBox();
+        }
+
+        private void UpdateListBox()
+        {
+            LinkedListBox.Items.Clear();
+
+            if (linkedList.Size > 0)
+            {
+                Node current = linkedList.Head;
+
+                do
+                {
+                    LinkedListBox.Items.Add(current.Data);
+                    current = current.Next;
+                } while (current != linkedList.Head);
+            }
+            else
+            {
+                LinkedListBox.Items.Add("List is empty");
+            }
+        }
+    }
+
+}
+
